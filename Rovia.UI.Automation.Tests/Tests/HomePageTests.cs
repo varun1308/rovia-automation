e7407a4a045a39486924cf913c32f2ab655f9ca2
vikul rathod <vrathod@tavisca.com>
2014-12-11 16:31:50 +0530

    test scenario changes
    1. kept only 1 row per sheet
    2. commented the home and hotel test classes

--- Rovia.UI.Automation.Tests/Tests/HomePageTests.cs
+++ Rovia.UI.Automation.Tests/Tests/HomePageTests.cs
@@ -1,51 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Rovia.UI.Automation.DataBinder;
-using Rovia.UI.Automation.ScenarioObjects;
-using Rovia.UI.Automation.Tests.Application;
-using Rovia.UI.Automation.Tests.Model;
-using Rovia.UI.Automation.Tests.Tests.AirTests;
-using Rovia.UI.Automation.Tests.Utility;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Threading;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Rovia.UI.Automation.DataBinder;
+//using Rovia.UI.Automation.ScenarioObjects;
+//using Rovia.UI.Automation.Tests.Application;
+//using Rovia.UI.Automation.Tests.Model;
+//using Rovia.UI.Automation.Tests.Tests.AirTests;
+//using Rovia.UI.Automation.Tests.Utility;
 
-namespace Rovia.UI.Automation.Tests.Tests
-{
-    [TestClass]
-    public class HomePageTests
-    {
-        public TestContext TestContext { get; set; }
+//namespace Rovia.UI.Automation.Tests.Tests
+//{
+//    [TestClass]
+//    public class HomePageTests
+//    {
+//        public TestContext TestContext { get; set; }
 
-        [ClassInitialize]
-        public static void ClassInitialize(TestContext testContext)
-        {
-            AirHappyFlowTest.DataBinder = new AirCriteriaDataBinder();
+//        [ClassInitialize]
+//        public static void ClassInitialize(TestContext testContext)
+//        {
+//            AirHappyFlowTest.DataBinder = new AirCriteriaDataBinder();
 
-        }
+//        }
 
-        [TestInitialize]
-        public void TestInitialize(){}
+//        [TestInitialize]
+//        public void TestInitialize(){}
 
-        [TestCleanup]
-        public void TestCleanup()
-        {
-            TestHelper.CleanUp();
-            TestHelper.GoToHomePage();
-        }
-
-        [TestMethod]
-        [TestCategory("Sanity")]
-        public void ShouldShowHomePage()
-        {
-            try
-            {
-                TestHelper.GoToHomePage();
-            }
-            catch (Exception exception)
-            {
-                Assert.Fail(exception.Message);
-            }
-        }
-    }
-}
+//        [TestCleanup]
+//        public void TestCleanup()
+//        {
+//            TestHelper.CleanUp();
+//            TestHelper.GoToHomePage();
+//        }
+//    }
+//}
