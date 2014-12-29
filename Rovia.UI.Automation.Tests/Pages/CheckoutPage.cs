904f6813a9ee72ffa62ea2d53c7032488ad63e59
aGuliya <aguliya@tavisca.com>
2014-12-29 15:46:56 +0530

    Added TripProduct Validation on CheckOutPage
    Added TripProductValidation BackBone
    Added HotelTripProductValidationImplementation

--- Rovia.UI.Automation.Tests/Pages/CheckoutPage.cs
+++ Rovia.UI.Automation.Tests/Pages/CheckoutPage.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using AppacitiveAutomationFramework;
 using OpenQA.Selenium;
 using Rovia.UI.Automation.Exceptions;
 using Rovia.UI.Automation.Logger;
 using Rovia.UI.Automation.ScenarioObjects;
+using Rovia.UI.Automation.ScenarioObjects.Hotel;
 using Rovia.UI.Automation.Tests.Configuration;
 
 namespace Rovia.UI.Automation.Tests.Pages
@@ -78,6 +82,74 @@ namespace Rovia.UI.Automation.Tests.Pages
                 throw new Alert(divErrors.Text);
         }
 
+        private void ValidateHotelTripProductDetails(HotelTripProduct hotelTripProduct, HotelResult hotelResult)
+        {
+            var errors = new StringBuilder();
+            if (!hotelResult.HotelName.Equals(hotelTripProduct.ProductTitle))
+                errors.Append(FormatError("HotelName", hotelResult.HotelName, hotelTripProduct.ProductTitle));
+            if (!hotelResult.HotelAddress.Replace(",", "").Equals(hotelTripProduct.Address.Replace(",", "")))
+                errors.Append(FormatError("HotelAddress", hotelResult.HotelAddress, hotelTripProduct.Address));
+            if (!hotelResult.Amount.Equals(hotelTripProduct.Fares.TotalFare))
+                errors.Append(FormatError("HotelPrice", hotelResult.Amount.ToString(), hotelTripProduct.Fares.TotalFare.ToString()));
+            if (!hotelResult.StayPeriod.Equals(hotelTripProduct.StayPeriod))
+                errors.Append(FormatError("StayPeriod", hotelResult.StayPeriod.ToString(), hotelTripProduct.StayPeriod.ToString()));
+            if (!hotelResult.SelectedRoom.NoOfRooms.Equals(hotelTripProduct.Room.NoOfRooms))
+                errors.Append(FormatError("NoOfRooms", hotelResult.SelectedRoom.NoOfRooms.ToString(), hotelTripProduct.Room.NoOfRooms.ToString()));
+            if (!string.IsNullOrEmpty(errors.ToString()))
+                throw new ValidationException(errors + "| on PaxInfoPage");
+        }
+
+        private string FormatError(string error, string addedValue, string tfValue)
+        {
+            return string.Format("| Invalid {0} ({1}, {2})", error, addedValue, tfValue);
+        }
+
+        private List<TripProduct> ParseTripProducts()
+        {
+            return GetUIElements("tripProducts").Select(x =>
+            {
+                switch (GetTripProductType(x))
+                {
+                    case TripProductType.Hotel:
+                        return ParseHotelTripProduct(x);
+                    default:
+                        return null;
+                }
+            }).ToList();
+        }
+
+        private TripProduct ParseHotelTripProduct(IUIWebElement tripProduct)
+        {
+            var stayperiod = tripProduct.GetUIElements("stayDates").Select(x => x.Text).ToArray();
+            return new HotelTripProduct()
+            {
+                ProductTitle = tripProduct.WaitAndGetBySelector("title", ApplicationSettings.TimeOut.Fast).Text,
+                Address = tripProduct.WaitAndGetBySelector("subTitle", ApplicationSettings.TimeOut.Fast).Text,
+                Rating = tripProduct.GetUIElements("rating").Count,
+                StayPeriod = new StayPeriod()
+                {
+                    CheckInDate = DateTime.Parse(stayperiod[0]),
+                    CheckOutDate = DateTime.Parse(stayperiod[1])
+                },
+                Fares = new Fare()
+                {
+                    TotalFare = new Amount(WaitAndGetBySelector("price", ApplicationSettings.TimeOut.Fast).Text)
+                },
+                Room = new HotelRoom()
+                {
+                    NoOfRooms = int.Parse(WaitAndGetBySelector("roomCount", ApplicationSettings.TimeOut.Fast).Text.Split()[0])
+                }
+            };
+        }
+
+        private TripProductType GetTripProductType(IUIWebElement tripProduct)
+        {
+            if (tripProduct.WaitAndGetBySelector("hotelStars", ApplicationSettings.TimeOut.Fast) != null)
+                return TripProductType.Hotel;
+            return TripProductType.Air;
+        }
+
+
         #endregion
 
         #region Protected Members
@@ -134,6 +206,19 @@ namespace Rovia.UI.Automation.Tests.Pages
                 LogManager.GetInstance().LogInformation("Payment Failed"); 
                 throw;
             }
+        }
+
+        public void ValidateTripDetails(Results selectedItineary)
+        {
+            ParseTripProducts().ForEach(x =>
+            {
+                switch (x.ProductType)
+                {
+                    case TripProductType.Hotel:
+                        ValidateHotelTripProductDetails(x as HotelTripProduct, selectedItineary as HotelResult);
+                        break;
+                }
+            });
         }
     }
 }
